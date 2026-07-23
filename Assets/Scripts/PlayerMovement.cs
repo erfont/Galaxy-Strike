4c82ab93838a2abceb0023036ba3793bf0c4b1e6
Jose Maria Font Fernandez <jose.font@mau.se>
2026-07-23 09:03:13 +0200

    Player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float controlSpeed = 50f;
+
+    Vector2 movement;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,20 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessTranslation();
+
+    }
+
+    public void OnMove(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+    }
+    private void ProcessTranslation()
+    {
+        float XOffset = movement.x * controlSpeed * Time.deltaTime;
+
+        float YOffset = movement.y * controlSpeed * Time.deltaTime;
+
+        transform.localPosition = new Vector3(transform.localPosition.x + XOffset, transform.localPosition.y + YOffset, 0f);
     }
 }

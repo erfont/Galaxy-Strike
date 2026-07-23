f041956cfe2bb447f5ee6a82ddc307179e6b890e
Jose Maria Font Fernandez <jose.font@mau.se>
2026-07-23 09:31:49 +0200

    ship rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,18 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float controlSpeed = 50f;
+
+    [SerializeField] float rotationSpeed = 8f;
+
+    [SerializeField] float controlRollFactor = 45f;
+
+    [SerializeField] float XOffsetClampMin = -19, XOffsetClampMax = 19, YOffsetClampMin = -8f, YOffsetClampMax= 20f;   
+
 
     Vector2 movement;
 
@@ -17,8 +26,10 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         ProcessTranslation();
+        ProcessRotation();
 
     }
+
 
     public void OnMove(InputValue value)
     {
@@ -26,10 +37,23 @@ public class PlayerMovement : MonoBehaviour
     }
     private void ProcessTranslation()
     {
+
+        
         float XOffset = movement.x * controlSpeed * Time.deltaTime;
+        float clampedNewXPos = Mathf.Clamp(transform.localPosition.x + XOffset, XOffsetClampMin, XOffsetClampMax);
 
         float YOffset = movement.y * controlSpeed * Time.deltaTime;
+        float clampedNewYPos = Mathf.Clamp(transform.localPosition.y + YOffset, YOffsetClampMin, YOffsetClampMax);
 
-        transform.localPosition = new Vector3(transform.localPosition.x + XOffset, transform.localPosition.y + YOffset, 0f);
+
+        transform.localPosition = new Vector3(clampedNewXPos, clampedNewYPos, 0f);
+    }
+    private void ProcessRotation()
+    {
+        Quaternion targetRotation = Quaternion.Euler(-controlRollFactor * movement.y, 0f, -controlRollFactor * movement.x);
+
+        Quaternion lerpedRotation = Quaternion.Lerp(transform.localRotation, targetRotation, Time.deltaTime * rotationSpeed);
+
+        transform.localRotation = lerpedRotation;
     }
 }

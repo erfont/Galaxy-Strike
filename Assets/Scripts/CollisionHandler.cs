6cdaf8ac62994a2d33f8aa1c7559e8bf9cd6ed72
Jose Maria Font Fernandez <jose.font@mau.se>
2026-07-27 13:03:18 +0200

    particle effx prefabbed

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,20 +2,12 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] GameObject destroyedVFX;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other) 
     {
-        Debug.Log("Hit "+ other.gameObject.name);
+        Instantiate(destroyedVFX, transform.position, Quaternion.identity); // Quaternion identity is the default 1, 1, 1 rotation
+        Destroy(this.gameObject);   
     }
 }

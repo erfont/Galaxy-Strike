1a988ba8751424704db8b6d25cb7c443c749f654
Jose Maria Font Fernandez <jose.font@mau.se>
2026-07-27 17:07:15 +0200

    enemy hitpoints

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,9 +3,24 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destroyedVFX;
-    private void OnParticleCollision(GameObject other) 
+    [SerializeField] GameObject hitVFX;
+    [SerializeField] int hitPoints = 4;
+
+    private void OnParticleCollision(GameObject other)
     {
-        Instantiate(destroyedVFX, transform.position, Quaternion.identity); // Quaternion identity is the default 1, 1, 1 rotation
-        Destroy(this.gameObject);    
+        ProcessHit();
+
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints--;
+        
+        if (hitPoints <= 0)
+        {
+            Instantiate(destroyedVFX, transform.position, Quaternion.identity); // Quaternion identity is the default 1, 1, 1 rotation
+            Destroy(this.gameObject);
+        } 
+        else Instantiate(hitVFX, transform.position, Quaternion.identity);
     }
 }

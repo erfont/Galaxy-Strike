d48852f01ab443ec50ecd2cbb647f54e18cbd2cd
Jose Maria Font Fernandez <jose.font@mau.se>
2026-07-29 09:16:04 +0200

    Music and explosion SFX

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -4,10 +4,18 @@ public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] GameObject destroyedVFX;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    GameSceneManager gameSceneManager;
 
+    void Start()
+    {
+        gameSceneManager = FindAnyObjectByType<GameSceneManager>();
+    }
     private void OnTriggerEnter(Collider other) 
     {
         Instantiate(destroyedVFX, transform.position, Quaternion.identity); // Quaternion identity is the default 1, 1, 1 rotation
+
+        gameSceneManager.ReloadLevel();
+
         Destroy(this.gameObject);   
     }
 }
